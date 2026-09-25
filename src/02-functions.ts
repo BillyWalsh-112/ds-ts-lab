b187dc6fbe826a9bdbdc87b213cd33cc577a054e
BillyWalsh112 <20107248@mail.wit.ie>
2026-09-25 12:00:17 +0100

    Higher-order functions

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,7 +1,8 @@
 import {Friend, Colleague } from './myTypes'
 import { friends, colleagues } from "./01-basics";
+import { EmailContact } from './myTypes';
 
-function older(f: Friend) : string {
+function older(f: Friend) {
      f.age += 1
      return `${f.name} is now ${f.age}` 
 }
@@ -9,37 +10,61 @@ function older(f: Friend) : string {
 console.log(older(friends[0]))
 
 // Find the colleague with the highest extension number.
-function highestExtension(cs: Colleague[]): Colleague {
+function highestExtension(cs: Colleague[]) { // Inferred retun type
   const result = cs.sort(
     (c1, c2) => c1.contact.extension - c2.contact.extension
   );
   return result[cs.length - 1];
 }
 
+
+console.log(highestExtension(colleagues.current));
+
+
 function addColleague(
   cs: Colleague[],
   name: string,
   department: string,
   email: string
-): Colleague {
-  const highest = highestExtension(cs);
+) {
 
-  const newColleague: Colleague = {
-    name: name,
-    department: department,
-    contact: {
-      email: email,
-      extension: highest.contact.extension + 1,
-    },
-  };
+const colleague = {
+  name: name,
+  department: department,
+  contact: {
+    email: email,
+    extension: highestExtension(cs).contact.extension + 1,
+  },
+};
 
-  cs.push(newColleague);
+  cs.push(colleague);
 
-  return newColleague;
+  return colleague;
 }
 
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 
 
-console.log(highestExtension(colleagues.current));
+function sortColleagues(
+  colleagues: Colleague[],
+  sorter: (c1: Colleague, c2: Colleague) => number
+): EmailContact[] {
+  const sorted = colleagues.sort(sorter); // Colleague[] inferred
+  const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+  return result 
+}
+
+console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
+console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+
+function findFriends(
+  friends: Friend[],
+  finder: (friend: Friend) => boolean
+) {
+  const result = friends.filter(finder);
+  return result;
+}
+
+console.log(findFriends(friends, (friend) => friend.name.startsWith("Pa")));
+console.log(findFriends(friends, (friend) => friend.age < 35));
